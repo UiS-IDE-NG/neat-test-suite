dfc52086c2c5a53909c41791978c65012e8d3d08
HelGum <helena.gummedal@gmail.com>
2020-05-15 13:08:53 +0200

    Remove replicate-graphs folder

--- bachelor-thesis/replicate-graphs/neat/sample_cpu.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-#include "neat.h"
-#include "neat_log.h"
-
-void log_cpu_time(char * filename, struct timespec *start, struct timespec *end, struct timespec *diff);
-//void sample_memory_usage(long int my_pid, char* name, struct neat_ctx *nc);
-void sample_memory_usage(char* name, struct neat_ctx *nc);
